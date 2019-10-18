6e45178d05096c0e89cf4d6dc1acc4ab4e159227
keun-park <53453831+keun-park@users.noreply.github.com>
2019-10-18 13:04:40 -0400

    Copied from https://gitlab.vrd-drv.crc.ca
    Copied from https://gitlab.vrd-drv.crc.ca/itim/ansible/WinRMtester/edit/master/winrmtest.py

--- winrmtest.py
+++ winrmtest.py
@@ -8,6 +8,10 @@
 from winrm.protocol import Protocol
 import sys
 
+#====================================================================================================
+#You can enter the IP for testing as the first arg eg "./winrmtest.py x.x.x.x" or it will ask you
+#If you are going to be using the tester for one IP only you canjust delete this and set the IP variable 
+
 try:
     ip = sys.argv[1]
     
@@ -15,12 +19,19 @@ except IndexError:
     ip = raw_input("Enter IP: ")
 
 print(ip)
+#====================================================================================================
+
 
 p = Protocol(
     endpoint='https://' + ip + ':5986/wsman',
     transport='ssl',
+
+#====================================================================================================
+#Username and Password on the VM you are testing 
     username='crcadmin',
     password='8dN3n2%bD73Z483!nbSg',
+#====================================================================================================
+
     server_cert_validation='ignore')
 shell_id = p.open_shell()
 command_id = p.run_command(shell_id, 'ipconfig', ['/all'])
